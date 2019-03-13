201f8c197e11c0392a4d8bc8686fca8d375e8f72
Tom Miller <thomas.j.c.miller@outlook.com>
2019-03-13 16:09:20 +0000

    get request :id

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,3 +25,10 @@ app.get('/', (req, res) => {
   }));
   res.send(qs);
 });
+
+app.get('/:id', (req, res) => {
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
