fe34f4e36ef1acd21c35c0a4572b0cd05ad9e15f
Tom Miller <thomas.j.c.miller@outlook.com>
2019-03-13 16:08:48 +0000

    question set up

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+const questions = [];
+
+app.use(helmet());
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(morgan('combined'));
+
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
